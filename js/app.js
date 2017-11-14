e683f4c438a2e4ec6254e2f3d54c64ca87c65d75
bl <874891428@qq.com>
2017-11-14 17:02:57 +0800

    首页的完善

--- js/app.js
+++ js/app.js
@@ -119,4 +119,68 @@ $(document).ready(function() {
 
 	});
 });
+window.onload = function() {
+	var pics = document.getElementById('pics_ol').getElementsByTagName('li');
+	var dots = document.getElementById("dots_ul").getElementsByTagName("li");
+	var luanpo = document.getElementById("carousel");
+	var oimg = document.getElementById('pics_ol');
+	var iNow = 0;
+	var dt = null;
+	var dot_over_color = "white"; //选中状态的颜色
+	var dot_out_color = "brown"; //没有选中的颜色
+	dots[0].style.backgroundColor = dot_over_color;
+	oimg.style.width = pics.length * pics[0].offsetWidth + "px";
 
+	function tab() {
+		for (var i = 0; i < dots.length; i++) {
+			dots[i].index = i;
+			//设置鼠标经过图片的动作
+			pics[i].onmouseover = function() {
+				clearInterval(dt);
+			}
+			pics[i].onmouseout = function() {
+				start();
+			}
+
+			//设置鼠标经过小圆点的动作
+			dots[i].onmouseover = function() {
+				clearInterval(dt);
+				iNow = this.index;
+				for (var i = 0; i < dots.length; i++) {
+					dots[i].className = "b";
+					dots[i].style.backgroundColor = dot_out_color;
+				}
+				this.className = "a";
+				this.style.backgroundColor = dot_over_color;
+				oimg.style.left = -(pics[0].offsetWidth * iNow) + "px"; //这边可以加上我前面所写的那个缓动框架            
+			}
+			dots[i].onmouseout = function() {
+				start();
+			}
+		}
+	}
+
+	function start() {　　
+		clearInterval(dt);
+		dt = setInterval(function() {
+			if (iNow > dots.length - 2) {
+				iNow = 0;
+			} else {
+				iNow++;
+			}
+			for (var k = 0; k < dots.length; k++) {
+				if (iNow == dots[k].index) {
+					dots[k].className = 'a';
+					dots[k].style.backgroundColor = dot_over_color;
+				} else {
+					dots[k].className = 'b';
+					dots[k].style.backgroundColor = dot_out_color;
+				}
+			}
+			oimg.style.left = -(pics[0].offsetWidth * iNow) + "px"; //这边可以加上我前面所写的那个缓动框架
+		}, 2000);
+		tab();
+	}
+
+	start();
+}
